bd03f389b8628799a83c3b3dd37871a4358eec6a
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-01-30 16:16:13 +0530

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,1 +1,15 @@
- 
+#!/bin/bash
+
+rm TOS_repo*
+
+echo "repo-add"
+#repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
+repo-add -s -n -R TOS_repo.db.tar.gz *.pkg.tar.zst
+repo-add -s -n -R TOS_repo.db.tar.gz *.pkg.tar.gz
+
+sleep 5
+#cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################" 
